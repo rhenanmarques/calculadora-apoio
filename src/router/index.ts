b926775afdb2274b9c3d193be4378a48defe2674
Renan Marques <rhenan_marx@hotmail.com>
2023-04-07 22:31:37 +0100

    alteração na rota

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,17 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-// import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     redirect: '/home'
   },
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomePage
+  }
 ]
 
 const router = createRouter({
